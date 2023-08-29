bf270f062b1349ef13b8e602c670ee52723d4b46
alejandroDi <yonathan.tovar@alumnos.udg.mx>
2023-08-29 13:23:15 -0600

    Entities add in the repo

---
+++ src/main/java/com/tienda/Test/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,10 @@
+package com.tienda.Test.persistence.entity;
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+
+}
