555daa120af62169b12598c63c17468310887555
alejandroDi <yonathan.tovar@alumnos.udg.mx>
2023-08-29 11:18:09 -0600

    first commit

---
+++ src/main/java/com/tienda/Test/TiendaTestApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/Test/TiendaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
